ceec99964204b9c08ac00c0a3b2b9182d8266814
Аслан <aslanmusin02@gmail.com>
2024-11-25 10:28:36 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,32 @@
 </script>
 
 <template>
-  <button type="button" class="btn btn-secondary">Secondary</button>
+<div class="container">
+  <div class="row">
+  <div class="col">
+    <h1 class="text-center ma-3">учет товаров</h1>
+      <div class="mb-3">
+        <label for="name" class="form-label">название</label>
+        <input type="text" class="form-control" id="name" >
+      </div>
+      <div class="mb-3">
+        <label for="date" class="form-label">Дата добавления </label>
+        <input type="date" class="form-control" id="date" >
+      </div>
+      <div class="mb-3">
+        <label for="count" class="form-label">количество</label>
+        <input type="numder" class="form-control" id="count" >
+      </div>
+      <div class="mb-3">
+        <label for="price" class="form-label">цена</label>
+        <input type="namber" class="form-control" id="price" >
+      </div>
+      <div>
+        <button type="button" class="btn btn-outline-success">Добавить</button>
+      </div>
+  </div>
+  </div>
+  </div>
 </template>
 
 
