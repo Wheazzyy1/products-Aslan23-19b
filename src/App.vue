a922f9264a941d583cbe7a944094b59984c92441
Аслан <aslanmusin02@gmail.com>
2024-11-25 10:38:34 +0500

    feat: add card list

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,46 @@
 
 <template>
 <div class="container">
-  <div class="row">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center my-3">Учет товаров</h1>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Название</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="date" class="form-label">Дата добавления</label>
+          <input type="date" class="form-control" id="date">
+        </div>
+        <div class="mb-3">
+          <label for="count" class="form-label">Количество</label>
+          <input type="number" class="form-control" id="date">
+        </div>
+        <div class="mb-3">
+          <label for="price" class="form-label">Цена</label>
+          <input type="number" class="form-control" id="date">
+        </div>
+        <div>
+          <button type="button" class="btn btn-outline-success">Добавить</button>
+        </div>
+      </div>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 mt-4">
   <div class="col">
-    <h1 class="text-center ma-3">учет товаров</h1>
-      <div class="mb-3">
-        <label for="name" class="form-label">название</label>
-        <input type="text" class="form-control" id="name" >
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">HP</h5>
+        <p class="card-text">$1000</p>
+        <p class="card-text">x10</p>
+        <p class="card-text">25.11.2024</p>
       </div>
-      <div class="mb-3">
-        <label for="date" class="form-label">Дата добавления </label>
-        <input type="date" class="form-control" id="date" >
+      <div class="card-footer">
+        <button class="btn btn-outline-danger">Удалить</button>
       </div>
-      <div class="mb-3">
-        <label for="count" class="form-label">количество</label>
-        <input type="numder" class="form-control" id="count" >
-      </div>
-      <div class="mb-3">
-        <label for="price" class="form-label">цена</label>
-        <input type="namber" class="form-control" id="price" >
-      </div>
-      <div>
-        <button type="button" class="btn btn-outline-success">Добавить</button>
-      </div>
+    </div>
   </div>
+</div>
   </div>
-  </div>
-</template>
+  </template>
 
 
